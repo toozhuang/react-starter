216da01c383d816c1149cb6738d767eaa71d9f40
toozhuang <wangz.today@gmail.com>
2022-03-15 09:11:03 +0800

    chore: import the webpack dev server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 console.log('我是大帅比')
+import './hello.scss'
--- webpack.config.js
+++ webpack.config.js
@@ -10,15 +10,25 @@ const friendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 
 module.exports = {
     mode: "development",
+    devtool: 'inline-source-map',
     // 项目入口， webpack 会从这个地方开始
     entry: {
         app:'./src/index.js'    // app 就会是下面的 [name] 的值
     },
-
     // output webpack 打包完成后的文件会在这里
     output:{
         path: path.resolve(__dirname,'./dist'),
         filename: "[name].bundle.js",   // 注意这个 name 会是我们打包的那个文件的名称（比如 index
+    },
+    // web dev server config
+    devServer: {
+        historyApiFallback: true,
+        static: {
+            directory:path.join(__dirname, './dist')
+        },
+        open: false,
+        hot: true,  // hot load ,检测文件的更改
+        port: 8082,
     },
     //loader
     module: {
@@ -33,6 +43,26 @@ module.exports = {
                         presets: ['@babel/preset-env'],
                     },
                 },
+            },
+            // CSS, PostCSS, and Sass
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', {
+                    loader: 'css-loader',
+                    options: {
+                        importLoaders: 1,
+                    },
+                }, 'postcss-loader'],
+            },
+            // Images
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            // Fonts and SVGs
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
             },
         ],
     },
