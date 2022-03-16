132e155b034dc37fed77c4fd872b5270eed34a90
toozhuang <wangz.today@gmail.com>
2022-03-16 17:37:59 +0800

    chore: 验证 hot reload on browser

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 /**
-* date: 2022-03-15, Tue, 9:17
-* author: Wang
-* feature： express take over the
-*/
+ * date: 2022-03-15, Tue, 9:17
+ * author: Wang
+ * feature： express take over the
+ */
 
 const express = require('express');
 const webpack = require('webpack');
@@ -15,12 +15,12 @@ const compiler = webpack(config);
 // Tell express to use the webpack-dev-middleware and use the webpack.config.js
 // configuration file as a base.
 app.use(
-    webpackDevMiddleware(compiler, {
-        publicPath: config.output.publicPath,
-    })
+  webpackDevMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+  })
 );
 
 // Serve the files on port 3000.
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!\n');
+  console.log('Example app listening on port 3000!\n');
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,90 +1,93 @@
 /**
-* date: 2022-03-14, Mon, 20:36
-* author: Wang
-* feature： Webpack 配置
-*/
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const {CleanWebpackPlugin}= require('clean-webpack-plugin')
+ * date: 2022-03-14, Mon, 20:36
+ * author: Wang
+ * feature： Webpack 配置
+ */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const friendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 
 module.exports = {
-    mode: "development",
-    devtool: 'inline-source-map',
-    // 项目入口， webpack 会从这个地方开始
-    entry: {
-        app:'./src/index.tsx'    // app 就会是下面的 [name] 的值
+  mode: 'development',
+  devtool: 'inline-source-map',
+  // 项目入口， webpack 会从这个地方开始
+  entry: {
+    app: './src/index.tsx', // app 就会是下面的 [name] 的值
+  },
+  // output webpack 打包完成后的文件会在这里
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js', // 注意这个 name 会是我们打包的那个文件的名称（比如 index
+    publicPath: '/', // 刚刚在这个地方懵逼了一下， 现在理解这个的作用
+    // 是 我们通过 browser 访问这个 项目的时候， 的入口
+    // 比如我设置 public path 为 /shuaibi
+    // 那么最后当我用 web dev middleware 结合 node epxress 访问的时候
+    // 就会是 localhost:端口号/shuaibi 这样
+  },
+  // web dev server config
+  devServer: {
+    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, './dist'),
     },
-    // output webpack 打包完成后的文件会在这里
-    output:{
-        path: path.resolve(__dirname,'./dist'),
-        filename: "[name].bundle.js",   // 注意这个 name 会是我们打包的那个文件的名称（比如 index
-        publicPath: "/" ,   // 刚刚在这个地方懵逼了一下， 现在理解这个的作用
-        // 是 我们通过 browser 访问这个 项目的时候， 的入口
-        // 比如我设置 public path 为 /shuaibi
-        // 那么最后当我用 web dev middleware 结合 node epxress 访问的时候
-        // 就会是 localhost:端口号/shuaibi 这样
-    },
-    // web dev server config
-    devServer: {
-        historyApiFallback: true,
-        static: {
-            directory:path.join(__dirname, './dist')
+    open: false,
+    hot: true, // hot load ,检测文件的更改
+    port: 8082,
+  },
+  //loader
+  module: {
+    rules: [
+      // JavaScript
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
         },
-        open: false,
-        hot: true,  // hot load ,检测文件的更改
-        port: 8082,
-    },
-    //loader
-    module: {
-        rules: [
-            // JavaScript
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                    },
-                },
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      // CSS, PostCSS, and Sass
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
             },
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            // CSS, PostCSS, and Sass
-            {
-                test: /\.(scss|css)$/,
-                use: ['style-loader', {
-                    loader: 'css-loader',
-                    options: {
-                        importLoaders: 1,
-                    },
-                }, 'postcss-loader'],
-            },
-            // Images
-            {
-                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-                type: 'asset/resource',
-            },
-            // Fonts and SVGs
-            {
-                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-                type: 'asset/inline',
-            },
+          },
+          'postcss-loader',
         ],
-    },
-    // 安装的 plugin 工具
-    plugins: [
-        new HtmlWebpackPlugin({
-            title:'大帅比的1f1',
-            template: path.resolve(__dirname,'./public/index.html'),
-            filename: "index.html"
-        }),
-        new CleanWebpackPlugin(),
-        new friendlyErrorsWebpackPlugin()
-    ]
-
-}
+      },
+      // Images
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      // Fonts and SVGs
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+    ],
+  },
+  // 安装的 plugin 工具
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '大帅比的1f1',
+      template: path.resolve(__dirname, './public/index.html'),
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new friendlyErrorsWebpackPlugin(),
+  ],
+};
